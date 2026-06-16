750a440bfb2d4a3c248dd257c7ac413d01372133
Amirtha Ganesh <amirthaganeshramesh@gmail.com>
2026-06-16 14:57:35 +0530

    Add document chunking

---
+++ src/splitter.py
@@ -0,0 +1,8 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def split_documents(documents):
+    splitter = RecursiveCharacterTextSplitter(chunk_size = 600,chunk_overlap =200, separators = ["\n\n","\n", "."," "] )
+    chunks = splitter.split_documents(documents)
+
+    return chunks
